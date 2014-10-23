ab659f0ef9383d23416d03e27082ef0a999b961d
Francesco Mondora <francesco.mondora@mondora.com>
2014-10-23 15:18:20 +0200

    Wordpress stuff

---
+++ wordpress/wp-content/themes/valledeitalenti/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="hu-HU" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="hu-HU" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html lang="hu-HU" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Blind spot radar app for cyclers and responsable car drivers. By installing the app you help prevent accidents while having audio and visual awareness about your surrounding">
+	<meta name="keywords" content="Bike, Safety, valledeitalenti.it, Shokame, blind spot, radar, ibeacon, startup, kitchen budapest, kibu, cyclist, shoka_me, iot, smart cities">
+	<title>valledeitalenti.it</title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="/xmlrpc.php">
+						<link rel="shortcut icon" href=>
+	<link rel="alternate" type="application/rss+xml" title="valledeitalenti.it &raquo; hírcsatorna" href="/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="valledeitalenti.it &raquo; hozzászólás hírcsatorna" href="/?feed=comments-rss2" />
+<link rel="alternate" type="application/rss+xml" title="valledeitalenti.it &raquo; Főoldal hozzászólás hírcsatorna" href="/?feed=rss2&#038;page_id=7" />
+<meta property="fb:admins" content="100001875003951"/><meta property="og:title" content="valledeitalenti.it"/><meta property="og:type" content="valledeitalenti.it"/><meta property="og:url" content=""/><meta property="og:image" content="/wp-content/themes/mobiltheme/images/screenshot4.png"/><meta property="og:site_name" content="valledeitalenti.it"/><meta property="og:description" content="valledeitalenti.it is a blind spot radar app for cyclers and car drivers. By installing the app you help prevent accidents while having audio and visual awareness about your surrounding and even more."/><meta property="og:image" content="/wp-content/themes/valledeitalenti/images/screen3.png"/>
+<link rel='stylesheet' id='mailchimp-for-wp-checkbox-css'  href='/wp-content/plugins/mailchimp-for-wp-pro/assets/css/checkbox.min.css?ver=2.4.4' type='text/css' media='all' />
+<link rel='stylesheet' id='mailchimp-for-wp-form-theme-dark-css'  href='/wp-content/plugins/mailchimp-for-wp-pro/assets/css/form-theme-dark.min.css?ver=2.4.4' type='text/css' media='all' />
+<link rel='stylesheet' id='twentythirteen-fonts-css'  href='//fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C700%2C300italic%2C400italic%2C700italic%7CBitter%3A400%2C700&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='/wp-content/themes/valledeitalenti/fonts/genericons.css?ver=2.09' type='text/css' media='all' />
+<link rel='stylesheet' id='twentythirteen-style-css'  href='/wp-content/themes/valledeitalenti/style.css?ver=2013-07-18' type='text/css' media='all' />
+<!--[if lt IE 9]>
+<link rel='stylesheet' id='twentythirteen-ie-css'  href='/wp-content/themes/valledeitalenti/css/ie.css?ver=2013-07-18' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='/wp-content/plugins/anything-popup/anything-popup.js?ver=3.9.2'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js?ver=1.11.0'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="/wp-includes/wlwmanifest.xml" /> 
+<link rel='next' title='Thank You' href='/?page_id=110' />
+<meta name="generator" content="WordPress 3.9.2" />
+<link rel='canonical' href='/' />
+<link rel='shortlink' href='/' />
+	<style type="text/css" id="twentythirteen-header-css">
+			.site-title,
+		.site-description {
+			position: absolute;
+			clip: rect(1px 1px 1px 1px); /* IE7 */
+			clip: rect(1px, 1px, 1px, 1px);
+		}
+			.site-header .home-link {
+			min-height: 0;
+		}
+		</style>
+		<link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,800,300,700' media="screen">
+    <link rel="stylesheet" type="text/css" href="wp-content/themes/valledeitalenti/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/less" href="wp-content/themes/valledeitalenti/css/style.less">
+    <script type="text/javascript" src="wp-content/themes/valledeitalenti/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="wp-content/themes/valledeitalenti/js/less-1.7.5.min.js"></script>
+	<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "8c0d4545-d9fd-430a-82fd-6bfc9c8f782e", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-543547a77be54161" async></script>
+
+</head>
+<body class="home page page-id-7 page-template-default single-author">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51393433-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	<div id="main-container" class="container">
+		    <div class="row">
+        <div class="col-md-6 col-md-offset-1 col-sm-6">
+            <h1>
+                ::1 - talento@127.0.0.1 
+            </h1>
+            <p class="description">
+                Uno spazio nuovo, tutto rivolto ai talenti e alle singole persone che vogliono condividere un ufficio con chi in Valtellina sta innovando nel mondo dell'Information Technology.
+            </p>
+            	<div id="secondary" class="sidebar-container" role="complementary">
+		<div class="widget-area">
+			<aside id="mc4wp_widget-2" class="widget widget_mc4wp_widget"><!-- MailChimp for WP Pro v2.4.4 --><div id="mc4wp-form-1" class="form mc4wp-form mc4wp-form-126"><form method="post" action="/"><p>
+    <label>Email Address:</label>
+    <input type="email" name="EMAIL" placeholder="Enter your email to see how" required="required">
+    <input type="submit" value="Ok">
+</p><textarea name="_mc4wp_required_but_not_really" style="display: none !important;"></textarea><input type="hidden" name="_mc4wp_form_id" value="126" /><input type="hidden" name="_mc4wp_form_instance" value="1" /><input type="hidden" name="_mc4wp_form_submit" value="1" /><input type="hidden" name="_mc4wp_form_nonce" value="ac4aa403b9" /></form></div><!-- / MailChimp for WP Pro --></aside>		</div><!-- .widget-area -->                  
+               
+      <br/><br/>
+      <ul class="description">
+          <li> una biblioteca IT <br></li>  
+          <!--br>
+          <li> un gruppo di persone altamente motivate<br></li>      
+          <li> un angolo relax in cui chattare di tecnologia<br></li>  
+          <li> una scrivania da noleggiare per le ore che si vogliono<br></li>
+          <li> un gruppo di amici che ti vogliono aiutare</li> 
+           -->
+      </ul>
+
+	</div><!-- #secondary -->
+        </div>
+        <div class="col-md-4 col-sm-6">
+			<a href="" title="valledeitalenti.it">
+	           <img src="/wp-content/themes/valledeitalenti/images/screen4.png" alt="valledeitalenti.it screen" class="phone-screen"/>
+	       </a>
+        </div>
+    </div>                      
+    
+     
+
+        <div class="row footer-row">
+            <div class="col-md-6 col-md-offset-1 col-sm-6">
+                <div class="kitchen"><a href="http://www.valtellina.it" target="_blank"><img src="/wp-content/themes/valledeitalenti/images/valtellina.png" alt="valledeitalenti.it" id="kitchenpic"/></a></div>
+                <div class="kitchen pirate"><a href="http://mondora.com/" target="_blank"><img src="/wp-content/themes/valledeitalenti/images/logo-mondora.jpg" alt="valledeitalenti.it" /></a></div>
+                <div class="kitchen pioneers"><a href="http://pioneers.io/festival/startups/roasted-2014" target="_blank"><img src="/wp-content/themes/valledeitalenti/images/pioneers.png" alt="valledeitalenti.it is selected as one of 3 startups that will pitch and get roasted on Pioneers Festival Academy stage!"/></a></div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                        <span class="contact">
+                        <a href="mailto:info@valledeitalenti.it?Subject=Inquiry about valledeitalenti.it" target="_top" style="color:white">
+                            Write to us</a>
+                        </span>
+                        <span class="followus">
+
+                            Follow us on: &nbsp <a href="https://twitter.com/shoka_me" target="_blank">
+                                <img src="/wp-content/themes/valledeitalenti/images/twittericon.png"style="margin-bottom:4px;"/>
+                            </a> &nbsp
+                            <a href="https://www.facebook.com/shokameapp" target="_blank">
+                                <img src="/wp-content/themes/valledeitalenti/images/facebookicon.png"style="margin-bottom:4px;"/>
+                            </a> &nbsp
+                            <a href="http://instagram.com/valledeitalenti.it" target="_blank">
+                                <img src="/wp-content/themes/valledeitalenti/images/instagramicon.png" style="margin-bottom:4px;"/>
+                            </a>
+                        </span>
+            </div>
+        </div>
+		</div><!-- #main-container -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+	<script type="text/javascript">
+				jQuery('.mc4wp-form').find('[type="submit"]').click(function () {
+					jQuery(this).parents('.mc4wp-form').addClass('mc4wp-form-submitted');
+				});
+			</script><script type='text/javascript' src='/wp-includes/js/comment-reply.min.js?ver=3.9.2'></script>
+<script type='text/javascript' src='/wp-includes/js/masonry.min.js?ver=3.1.2'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery.masonry.min.js?ver=3.1.2'></script>
+<script type='text/javascript' src='/wp-content/themes/valledeitalenti/js/functions.js?ver=2014-03-18'></script>
+</body>
+</html>
